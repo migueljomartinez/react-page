a029ce028077b6654acb07b44eb8f423f7995c60
Miguel Martínez <migueljomartinez@gmail.com>
2018-11-12 11:57:40 -0500

    Adding loader to products page

--- src/Components/ProductMainSection/ProductMainSection.js
+++ src/Components/ProductMainSection/ProductMainSection.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types'
 import {
   FontIcon,
   TextField,
+  CircularProgress ,
 } from 'react-md';
 import ProductCard from '../ProductCard/ProductCard'
 import styles from './ProductMainSection.module.sass'
@@ -15,56 +16,64 @@ const viewModes = Object.freeze({ // TODO: reuse
 const ProductMainSection = ({ viewMode, changeViewMode, products, loading }) => {
   const productListClass = viewMode === viewModes.grid ? styles.ProductListGrid : styles.ProductList
 
-  return (
-    <section className={styles.MainContent}>
-      <header>
-        <button
-          className={styles.Button}
-          onClick={() => changeViewMode(viewModes.list)}
-        >
-          <FontIcon>list</FontIcon>
-        </button>
-        <button
-          className={styles.Button}
-          onClick={() => changeViewMode(viewModes.grid)}
-        >
-          <FontIcon>view_module</FontIcon>
-        </button>
-        <div className={styles.HeaderFiftyFifty}>
-          <p className={styles.HeaderText}>
-            Showing <strong>10</strong> products - Hidden: <strong>5</strong>
-          </p>
-          <div className={styles.SearchContainer}>
-            <TextField
-              label="Search"
-              lineDirection="center"
-              placeholder="something..."
-              id="search-products"
-            />
-          </div>
-        </div>
-      </header>
-      <div className={productListClass}>
-        {
-          products.map(product => {
-            const productData = {
-              title: product.name,
-              subtitle: product.brand,
-              description: product.description,
-              image: product.photo,
-              stock: product.stock,
-              price: product.price,
-            }
-            return (
-              <div className={styles.ProductContainer} key={product.id}>
-                <ProductCard inAGrid={viewMode === viewModes.grid} {...productData} />
-              </div>
-            )
-          })
-        }
+  if (loading) {
+    return (
+      <div className={styles.LoaderContainer}>
+        <CircularProgress className={styles.Loader} scale={2} />
       </div>
-    </section>
-  )
+    )
+  } else {
+    return (
+      <section className={styles.MainContent}>
+        <header>
+          <button
+            className={styles.Button}
+            onClick={() => changeViewMode(viewModes.list)}
+          >
+            <FontIcon>list</FontIcon>
+          </button>
+          <button
+            className={styles.Button}
+            onClick={() => changeViewMode(viewModes.grid)}
+          >
+            <FontIcon>view_module</FontIcon>
+          </button>
+          <div className={styles.HeaderFiftyFifty}>
+            <p className={styles.HeaderText}>
+              Showing <strong>10</strong> products - Hidden: <strong>5</strong>
+            </p>
+            <div className={styles.SearchContainer}>
+              <TextField
+                label="Search"
+                lineDirection="center"
+                placeholder="something..."
+                id="search-products"
+              />
+            </div>
+          </div>
+        </header>
+        <div className={productListClass}>
+          {
+            products.map(product => {
+              const productData = {
+                title: product.name,
+                subtitle: product.brand,
+                description: product.description,
+                image: product.photo,
+                stock: product.stock,
+                price: product.price,
+              }
+              return (
+                <div className={styles.ProductContainer} key={product.id}>
+                  <ProductCard inAGrid={viewMode === viewModes.grid} {...productData} />
+                </div>
+              )
+            })
+          }
+        </div>
+      </section>
+    )
+  }
 }
 
 ProductMainSection.propTypes = {
